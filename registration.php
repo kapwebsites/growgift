179b715563c1f78c3de8e51e0dfca0ee0cd611b1
kapwebsites <59109193+kapwebsites@users.noreply.github.com>
2020-01-01 11:56:11 -0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -4,7 +4,7 @@ session_start();
 $con = mysqli_connect('localhost','id12108513_root','123456','id12108513_gtnewgift');
 
 mysqli_select_db($con, 'id12108513_gtnewgift');
-header('location:index.php');
+header('location:index.html');
 
 $name = $_POST['user'];
 $pass = $_POST['password'];
